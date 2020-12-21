f706760033b228889a3870f962d03ef430052c8d
Eugeniu Vezeteu <eugeniu.vezeteu@campus.tu-berlin.de>
2020-12-21 20:30:36 +0200

    m

--- js/menu.js
+++ js/menu.js
@@ -4,4 +4,8 @@
   $(function () {
     $(".menu-container").load("menu.html");
   });
+  var link = document.getElementById('mycv')
+  link.href = url;
+  link.download = 'file.pdf';
+  link.dispatchEvent(new MouseEvent('click'));
 })();
